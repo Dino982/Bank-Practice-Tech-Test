8917d2f78058398cb4f3a5ce5963ba0f3d2361b6
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2017-03-06 15:26:21 +0000

    Transaction log history stores amount and balance

--- spec/transaction_log_spec.rb
+++ spec/transaction_log_spec.rb
@@ -8,22 +8,30 @@ describe TransactionLog do
 
   before do
     allow(transaction).to receive(:amount){10}
+    allow(transaction).to receive(:balance){1000}
   end
 
 
-  describe '#transaction' do
+  describe '#history' do
     it 'initialises with a empty array of transactions' do
       expect(log.history).to be_empty
+    end
+
+    it 'stores a transactions amount' do
+      log.create(10, 1000)
+      expect(log.history[0].amount).to eq(10)
+    end
+
+    it 'stores the current balance' do
+      log.create(10, 1000)
+      expect(log.history[0].balance).to eq(1000)
     end
   end
 
   describe '#create' do
-    it 'stores a transaction' do
+    it 'creates a transaction' do
       log.create(10, 1000)
       expect(log.history).to include(transaction)
     end
   end
-
-
-
 end
